3b665846baaeceae1acdf16c4965c9c53e0edc90
Dima Parkhomenko <parhomenko.dmit@gmail.com>
2022-06-09 17:15:10 +0300

    Create config.php file

---
+++ source-file/config/config.php
@@ -0,0 +1,20 @@
+<?php
+namespace App;
+
+use PDO;
+
+return [
+    'database' => [
+        'user' => 'root',
+        'password' => 'password',
+        'name' => 'mysql:host=mysql;port:3306;',
+        'db' => 'MemberList',
+        'options' => [
+            PDO::ATTR_DEFAULT_FETCH_MODE => 2
+        ]
+    ],
+    'shareMessage' => [
+        'message' => 'Check out this Meetup with SoCal AngularJS!'
+    ]
+
+];
