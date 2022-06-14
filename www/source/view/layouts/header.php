43af0f70f9d02facc6fabece5724ab42850131ec
Dima Parkhomenko <parhomenko.dmit@gmail.com>
2022-06-14 16:26:39 +0300

    Page Icon added.

--- www/source/view/layouts/header.php
+++ www/source/view/layouts/header.php
@@ -8,6 +8,7 @@ namespace App\source\view\layout;
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="source/uploads/icon.png">
     <link href="source/view/index.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
